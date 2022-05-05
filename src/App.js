6e8aaacf8aea6830c7a52c86583d46c9f21849ed
rathak18 <rajendrapathak1988@gmail.com>
2022-05-05 10:00:44 +0530

    deployed filmland version 4

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SearchIcon from './search.svg';
 
 //abfac9c
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=abfac9c'
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=abfac9c'
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState("");
